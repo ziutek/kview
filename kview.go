078d900e9f702f28689547ea4d841dbe9e37ac7a
Michał Derkacz <michal@Lnet.pl>
2013-02-07 22:15:26 +0100

    Fix problem with Globals

--- kview.go
+++ kview.go
@@ -21,101 +21,9 @@ var (
 	}
 )
 
-type View interface {
-	Copy() View
-	Strict(bool)
-	Div(string, View)
-	Exec(io.Writer, ...interface{})
-	Render(...interface{}) *kasia.NestedTemplate
-}
-
-// View definition
-type KView struct {
-	name    string
-	tpl     *kasia.Template
-	globals map[string]interface{}
-}
-
-// Returns a pointer to the page
-func New(name string, globals ...map[string]interface{}) *KView {
-	var (
-		pg  KView
-		err error
-	)
-	pg.name = name
-	pg.tpl, err = kasia.ParseFile(path.Join(TemplatesDir, name))
-	if err != nil {
-		ErrorHandler(name, err)
-	}
-	pg.globals = make(map[string]interface{})
-	// First some default globals
-	for k, v := range Globals {
-		pg.globals[k] = v
-	}
-	// globals may default
-	for _, g := range globals {
-		for k, v := range g {
-			pg.globals[k] = v
-		}
-	}
-	return &pg
-}
-
-// Returns a copy of the page
-func (pg *KView) Copy() View {
-	new_pg := *pg
-	// Make a copy of globals
-	new_pg.globals = make(map[string]interface{})
-	for k, v := range pg.globals {
-		new_pg.globals[k] = v
-	}
-	return &new_pg
-}
-
-// Set the strict render flag
-func (pg *KView) Strict(strict bool) {
-	pg.tpl.Strict = strict
-}
-
-// Add subview
-func (pg *KView) Div(name string, view View) {
-	pg.globals[name] = view
-}
-
-func prepend(slice []interface{}, pre ...interface{}) (ret []interface{}) {
-	ret = make([]interface{}, len(slice)+len(pre))
-	copy(ret, pre)
-	copy(ret[len(pre):], slice)
-	return
-}
-
-// Render view to wr with data
-func (pg *KView) Exec(wr io.Writer, ctx ...interface{}) {
-	// Add globals to the bottom of the context stack
-	ctx = prepend(ctx, pg.globals)
-	err := pg.tpl.Run(wr, ctx...)
-	if err != nil {
-		ErrorHandler(pg.name, err)
-	}
-}
-
-// Use this method in template text to render page inside other page.
-func (pg *KView) Render(ctx ...interface{}) *kasia.NestedTemplate {
-	if len(ctx) > 0 {
-		// Check if render was called with full template context as first arg
-		if ci, ok := ctx[0].(kasia.ContextItself); ok {
-			// Rearange context, remove old globals
-			ctx = append(ci[1:], ctx[1:])
-		}
-	}
-	// Add globals to the bottom of the context stack
-	ctx = prepend(ctx, pg.globals)
-	return pg.tpl.Nested(ctx...)
-}
-
-// Some useful functions for globals.
-// You can add there your functions/variables which will be visable in any
-// view. See also globals parameter in New function.
+// Globals can store useful things that are global for all views.
+// You can add there your functions/variables which will be visible in any
+// view. See also symbols parameter in New function.
 var Globals = map[string]interface{}{
 	"len": func(a interface{}) int {
 		v := reflect.ValueOf(a)
@@ -124,8 +32,8 @@ var Globals = map[string]interface{}{
 		}
 		return -1
 	},
-	"fmt": fmt.Sprintf,
-	"join" : strings.Join,
+	"fmt":  fmt.Sprintf,
+	"join": strings.Join,
 	"add": func(a, b int) int {
 		return a + b
 	},
@@ -150,4 +58,96 @@ var Globals = map[string]interface{}{
 	"not": func(a bool) bool {
 		return !a
 	},
+}
+
+type View interface {
+	Copy() View
+	Strict(bool)
+	Div(string, View)
+	Exec(io.Writer, ...interface{})
+	Render(...interface{}) *kasia.NestedTemplate
+}
+
+// View definition
+type KView struct {
+	name    string
+	tpl     *kasia.Template
+	symbols map[string]interface{}
+}
+
+// Returns a pointer to the view
+func New(name string, symbols ...map[string]interface{}) *KView {
+	var (
+		pg  KView
+		err error
+	)
+	pg.name = name
+	pg.tpl, err = kasia.ParseFile(path.Join(TemplatesDir, name))
+	if err != nil {
+		ErrorHandler(name, err)
+	}
+	pg.symbols = make(map[string]interface{})
+	for _, g := range symbols {
+		for k, v := range g {
+			pg.symbols[k] = v
+		}
+	}
+	return &pg
+}
+
+// Returns a copy of the view
+func (pg *KView) Copy() View {
+	newpg := *pg
+	// Make a copy of globals
+	newpg.symbols = make(map[string]interface{})
+	for k, v := range pg.symbols {
+		newpg.symbols[k] = v
+	}
+	return &newpg
+}
+
+// Set the strict render flag
+func (pg *KView) Strict(strict bool) {
+	pg.tpl.Strict = strict
+}
+
+// Add subview
+func (pg *KView) Div(name string, view View) {
+	pg.symbols[name] = view
+}
+
+func prepend(slice []interface{}, pre ...interface{}) (ret []interface{}) {
+	ret = make([]interface{}, 0, len(slice)+len(pre))
+	ret = append(ret, pre...)
+	ret = append(ret, slice...)
+	return
+}
+
+// Render view to w with data
+func (pg *KView) Exec(w io.Writer, ctx ...interface{}) {
+	// Add globals to the bottom of the context stack
+	ctx = prepend(ctx, Globals, pg.symbols)
+	err := pg.tpl.Run(w, ctx...)
+	if err != nil {
+		ErrorHandler(pg.name, err)
+	}
+}
+
+// Use this method in template text to render view inside other view.
+func (pg *KView) Render(ctx ...interface{}) *kasia.NestedTemplate {
+	if len(ctx) > 0 {
+		// Check if render was called with full template context as first arg
+		if ci, ok := ctx[0].(kasia.ContextItself); ok {
+			// Rearange context
+			newctx := make([]interface{}, 0, len(ci)+len(ctx)-1)
+			newctx = append(newctx, ci...)
+			newctx = append(newctx, ctx[1:]...)
+			// Replace symbols
+			newctx[1] = pg.symbols
+			return pg.tpl.Nested(newctx...)
+		}
+	}
+	// Add globals to the bottom of the context stack
+	ctx = prepend(ctx, Globals, pg.symbols)
+	return pg.tpl.Nested(ctx...)
 }
